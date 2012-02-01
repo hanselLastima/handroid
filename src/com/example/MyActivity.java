d5b9c88108c68015d8efd0743711a50e9793699b
Hansel Ian I. Lastima <lastimah@hiro-kun.(none)>
2012-02-02 00:36:38 +0800

    Created a simple to-do list

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,6 +2,14 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyActivity extends Activity
 {
@@ -11,5 +19,31 @@ public class MyActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> itemList = new ArrayList<String>();
+        final ArrayAdapter<String> arrayAdapter;
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+
+        listView.setAdapter(arrayAdapter);
+        
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if(keyEvent.getAction() == keyEvent.ACTION_DOWN)
+                    if(keyCode == keyEvent.KEYCODE_ENTER) {
+                        String newItem = editText.getText().toString();
+                        itemList.add(0, newItem);
+                        arrayAdapter.notifyDataSetChanged();
+                        editText.setText("");
+                        return true;
+                    }
+                return false;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+
     }
 }
